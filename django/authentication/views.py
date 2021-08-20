c4fde03f539a8b8b624462db7e269d949eabb900
Prince Boamah <regioths@gmail.com>
2021-08-20 16:55:35 +0000

    Prince: Added login function to view

--- django/authentication/views.py
+++ django/authentication/views.py
@@ -17,8 +17,73 @@ def login_page(request):
 
 
 
-def login_user(request):
-    pass
+def login_in(request):
+    """Logs the user in
+    Retrieves the username and password provided by the
+    user after validating the form. The username and
+    password is authenticated, and if successful, the
+    user is logged into the current session.
+    If the authentication fails, or the form data provided
+    is invalid, or the data provided already exist, the
+    user is presented with the form and the appropriate
+    error message.
+    :param request: The WSGIRequest object for the current session
+    :return HttpResponse:
+    """
+    if request.user.is_authenticated:
+        if request.user.user_type == 'ADMIN':
+            return redirect('core:admin_home')
+        else:
+            return redirect('core:user_home')
+
+    if request.method == "POST":
+
+        # Get the login form with the data bound to it
+        form = LoginForm(request.POST)
+
+        # check to see if the data provided is valid.
+        if form.is_valid():
+
+            # If the data is valid, retrieve it.
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            # check to ensure that the username is not None.
+            # If it is, inform the user accordingly and presented
+            # with the form to try again
+            if username is not None and len(username) != 0:
+
+                # authenticate the user. If the user object is not None,
+                # it means the user exists in the system. Then if the password
+                # check passes, the user is logged in and redirected to their
+                # dashboard.
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    if user.user_type == 'ADMIN':
+                        user.check_password(password)
+                        login(request, user)
+                        messages.success(request, "You've been successfully logged in! ")
+                        return redirect('core:admin_home')
+                    else:
+                        user.check_password(password)
+                        login(request, user)
+                        messages.success(request, "You've been successfully logged in! ")
+                        return redirect('core:user_home')
+                else:
+                    messages.warning(request, "Username and/or password does not exist")
+                    return render(request, "authentication/login.html", {
+                        'form': form
+                    })
+    else:
+        # For a GET request, create a login form ad pass it
+        # to the login page for rendering.
+        form = LoginForm()
+
+    # render the login page along with the form
+        return render(request, "authentication/login.html", {
+            'form': form
+        })
+
 
 
 def registration(request):
