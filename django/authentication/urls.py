85655f96070b1729bdcf5645146fd2301bf020d4
Prince Boamah <regioths@gmail.com>
2021-08-22 23:14:58 +0000

    Prince: Added urls for login, registration and logout functions

--- django/authentication/urls.py
+++ django/authentication/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 
-from authentication.views import login_page
+from authentication.views import login_page, registration, logout, login_user
 
 urlpatterns = [
-    path('login/', login_page)
+    path('login/', login_page, name="login"),
+    path('signup/', registration, name="signup_page"),
+    path('logout/', logout, name="logout")
 ]
