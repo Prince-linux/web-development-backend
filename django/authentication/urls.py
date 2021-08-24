882609c76f882c8ededd2762d3e345900c13fac0
Prince Boamah <regioths@gmail.com>
2021-08-24 23:25:18 +0000

    Prince: Added appname authentication to urls

--- django/authentication/urls.py
+++ django/authentication/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, include
 
 from authentication.views import login_page, registration, logout, login_user
 
+
+app_name = 'authentication'
+
 urlpatterns = [
     path('login/', login_page, name="login"),
     path('signup/', registration, name="signup_page"),
