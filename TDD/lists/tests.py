9c4f0105659393f21a186c765fe2aa151225828d
Prince Boamah <regioths@gmail.com>
2022-01-25 15:03:38 +0000

    Test to check whether the home_page view passes

--- TDD/lists/tests.py
+++ TDD/lists/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
-
+from django.http import HttpRequest
 from lists.views import home_page
 # Create your tests here.
 
@@ -9,3 +9,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest
+        response = home_page(request)
+        html = response.content.decode('utf-8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do list</title>', html)
+        self.assertTrue(html.endswith('</html>'))
