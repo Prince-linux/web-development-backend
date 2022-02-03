b5245108d10c5339f7cc96afc3d2e0ebd9c1a0ce
Prince Boamah <regioths@gmail.com>
2022-02-03 14:31:27 +0000

    Form and user input test fails

--- TDD/lists/views.py
+++ TDD/lists/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', ''), })
